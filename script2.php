267cc938288130a2060668936577f04c9755f948
stage <stage@HP-XW6200.admin-services.local>
2014-01-24 11:44:54 +0100

    commit 24/01/14 11:44

--- script2.php
+++ script2.php
@@ -1,7 +1,6 @@
 <?php 
 
-$nomFichier = "C:\wamp\www\csv\Contact.csv";
-$nomFichierOutput = "contact.sql";
+$nomFichier = "C:\wamp\www\csv\Classeur1.csv";
 $nomTable = "contact";
 
 $openFic = fopen($nomFichier, "r");
@@ -9,12 +8,14 @@ $resulFic = fopen("C:\wamp\www\csv\contact.sql", "w");
 
 $first = true;
 $second = false;
-$reqBase = "INSERT INTO `contact` (`id_contact`, `civilite`, `nom_contact`, `prenom_contact`, `adresse`, `suite`, `cp`, `ville`, `societe`, `APE`, `pays`, `telephone`, `telephone2`, `telecopie`, `portable`, `site_web`, `email`, `origine`, `observations`, `date_creation`, `prospect`, `demandeur`, `offreur`, `investisseur`, `prescripteur`, `autre`, `confrere`, `date_dernier_contact`, `fonction`, `nom_responsable`, `origine_contact`, `support_publicitaire`, `contact_origine`, `accord_financier_origine`, `notaire`, `syndic`, `forme_juridique`, `agent`, `capital`, `RCS`, `lieu_RCS`, `siege`, `id_agence`) VALUES ";
+$reqBase = "INSERT INTO `contact` (`id_contact`, `civilite`, `nom_contact`, `prenom_contact`, `adresse`, `suite`, `cp`, `ville`, `societe`, `APE`, `pays`, `telephone`, `telephone2`, `telecopie`, `portable`, `site_web`, `email`, `origine`, `observations`, `date_creation`, `prospect`, `demandeur`, `offreur`, `investisseur`, `prescripteur`, `autre`, `confrere`, `date_dernier_contact`, `fonction`, `nom_responsable`, `origine_contact`, `support_publicitaire`, `contact_origine`, `accord_financier_origine`, `notaire`, `syndic`, `forme_juridique`, `agent`, `capital`, `RCS`, `lieu_RCS`, `siege`) VALUES ";
 
+$compteur = 0;
+$limit = 25;
 
-
-while(!feof($openFic)){
+while(!feof($openFic) && $compteur < $limit){
   $ligne = fgets($openFic);
+	$compteur = $compteur + 1;
 	//echo "ligne : ".$ligne;
 	if ($second) {
     $reqBase=$reqBase . ',';
